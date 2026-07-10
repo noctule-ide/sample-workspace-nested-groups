cb61fbad76499ceeb978ef81de4f1ee657c61dfa
jansorg <github@ja-dev.eu>
2026-07-10 15:21:08 +0200

    first commit

---
+++ iOS/iOS-app/iOS-app/iOS_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_appApp.swift
+//  iOS-app
+//
+//  Created by Joachim Ansorg on 10.07.26.
+//
+
+import SwiftUI
+
+@main
+struct iOS_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iOS/iOS-app/iOS-appTests/iOS_appTests.swift
@@ -0,0 +1,19 @@
+//
+//  iOS_appTests.swift
+//  iOS-appTests
+//
+//  Created by Joachim Ansorg on 10.07.26.
+//
+
+import Testing
+@testable import iOS_app
+
+struct iOS_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
